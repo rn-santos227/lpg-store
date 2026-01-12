f08003b7181bcab2aaab30d8f7d2d19759d01101
rn-santos227 <rn_santos227@yahoo.com>
2026-01-12 10:53:10 +0800

    updated order schema in sanity project

--- sanity/schemaTypes/order.ts
+++ sanity/schemaTypes/order.ts
@@ -5,6 +5,24 @@ export default {
   title: 'Order',
   type: 'document',
   fields: [
-
+    {
+      name: 'customer',
+      title: 'Customer',
+      type: 'reference',
+      to: [{ type: 'customer' }],
+      validation: (Rule: Rule) => Rule.required(),
+    },
+    {
+      name: 'customerSnapshot',
+      title: 'Customer Snapshot',
+      type: 'object',
+      fields: [
+        { name: 'name', type: 'string' },
+        { name: 'phone', type: 'string' },
+        { name: 'address', type: 'text' },
+        { name: 'location', type: 'geopoint' },
+      ],
+      description: 'Frozen customer data at time of order',
+    },
   ],
 }
