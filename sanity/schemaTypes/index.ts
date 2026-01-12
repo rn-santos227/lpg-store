220d97bc56137f6b3c45544bc4f6d78af768080a
rn-santos227 <rn_santos227@yahoo.com>
2026-01-12 10:56:26 +0800

    updated schema type index in santy project

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,1 +1,13 @@
-export const schemaTypes = []
+import category from './category'
+import customer from './customer'
+import inventory from './inventory'
+import product from './product'
+import order from './order'
+
+export const schemaTypes = [
+  category,
+  customer,
+  product,
+  inventory,
+  order,
+]
