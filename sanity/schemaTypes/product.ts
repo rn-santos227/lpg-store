483d4f3928d0c2bcba0c99cac2723399afbfa34d
rn-santos227 <rn_santos227@yahoo.com>
2026-01-12 10:44:10 +0800

    updated product schema in sanity project

--- sanity/schemaTypes/product.ts
+++ sanity/schemaTypes/product.ts
@@ -5,6 +5,11 @@ export default {
   title: 'Product',
   type: 'document',
   fields: [
-
+    {
+      name: 'name',
+      title: 'Product Name',
+      type: 'string',
+      validation: (Rule: Rule) => Rule.required(),
+    },
   ],
 }
