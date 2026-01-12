6f99ab4d61ac45ca53b3dbf048d0ffa8b04b866a
rn-santos227 <rn_santos227@yahoo.com>
2026-01-12 10:41:11 +0800

    updated category schema in sanity project

--- sanity/schemaTypes/category.ts
+++ sanity/schemaTypes/category.ts
@@ -1,5 +1,15 @@
+import type { Rule } from 'sanity'
+
 export default {
   name: 'category',
   title: 'Category',
   type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Category Name',
+      type: 'string',
+      validation: (Rule: Rule) => Rule.required()
+    },
+  ],
 }
