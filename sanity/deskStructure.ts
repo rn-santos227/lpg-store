981802aefc2c1c0880431b7b0e3574441bce3c72
rn-santos227 <rn_santos227@yahoo.com>
2026-01-13 10:28:14 +0800

    updated desk structure file in sanity root folder

--- sanity/deskStructure.ts
+++ sanity/deskStructure.ts
@@ -4,5 +4,29 @@ export const structure: StructureResolver = (S) =>
   S.list()
     .title('A4R LPG Admin')
     .items([
-      
+      S.listItem()
+        .title('Operations')
+        .child(
+          S.list()
+            .title('Operations')
+            .items([
+              S.listItem()
+                .title('Orders')
+                .schemaType('order')
+                .child(
+                  S.documentTypeList('order')
+                    .title('Orders')
+                    .defaultOrdering([{ field: 'createdAt', direction: 'desc' }])
+                ),
+
+              S.listItem()
+                .title('Customer Inquiries')
+                .schemaType('inquiry')
+                .child(
+                  S.documentTypeList('inquiry')
+                    .title('Customer Inquiries')
+                    .defaultOrdering([{ field: 'createdAt', direction: 'desc' }])
+                ),
+            ])
+        ),
     ])
