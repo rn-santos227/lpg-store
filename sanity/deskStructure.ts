78a11d3fad62ecc00814d4adbe5ef7d9d9ca6d80
rn-santos227 <rn_santos227@yahoo.com>
2026-01-13 10:27:33 +0800

    updated desk structure file in sanity root folder

--- sanity/deskStructure.ts
+++ sanity/deskStructure.ts
@@ -1,3 +1,8 @@
 import type { StructureResolver } from 'sanity/structure'
 
-
+export const structure: StructureResolver = (S) =>
+  S.list()
+    .title('A4R LPG Admin')
+    .items([
+      
+    ])
