88dacb0b96d00936384cf5a3c12499afedd602cb
rn-santos227 <rn_santos227@yahoo.com>
2026-01-13 11:00:58 +0800

    updated tailwind config file

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -2,8 +2,8 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './src/app/**/*.{ts,tsx}',
-    './src/components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
   ],
   theme: {
     extend: {},
