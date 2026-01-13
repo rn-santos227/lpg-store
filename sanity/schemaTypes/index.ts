5fd23f82228e286ad8ef24bda7f7c8809fd23fb2
rn-santos227 <rn_santos227@yahoo.com>
2026-01-13 10:24:11 +0800

    updated index file in schema types folder

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,6 @@
 import category from './category'
 import customer from './customer'
+import inquiry from "./inquiry"
 import inventory from './inventory'
 import product from './product'
 import order from './order'
@@ -8,6 +9,7 @@ export const schemaTypes = [
   category,
   customer,
   product,
+  inquiry,
   inventory,
   order,
 ]
