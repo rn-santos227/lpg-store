b104cfa7000d767d707bddfd2c71cde7942e9845
rn-santos227 <rn_santos227@yahoo.com>
2026-01-14 13:06:13 +0800

    updated slide show ui component file

--- web/components/ui/SlideShow.tsx
+++ web/components/ui/SlideShow.tsx
@@ -12,4 +12,18 @@ import {
   useState,
 } from "react";
 
+type TooltipPlacement = "top" | "bottom" | "left" | "right";
 
+type TooltipProps<T extends HTMLElement = HTMLElement> = {
+  content: ReactNode;
+  children: ReactElement<HTMLAttributes<T>>;
+  placement?: TooltipPlacement;
+  className?: string;
+};
+
+const placementClasses: Record<TooltipPlacement, string> = {
+  top: "bottom-full left-1/2 -translate-x-1/2 mb-2",
+  bottom: "top-full left-1/2 -translate-x-1/2 mt-2",
+  left: "right-full top-1/2 -translate-y-1/2 mr-2",
+  right: "left-full top-1/2 -translate-y-1/2 ml-2",
+};
