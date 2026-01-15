2a1a7336da3887db52b202d313dd0f61869e3eda
rn-santos227 <rn_santos227@yahoo.com>
2026-01-15 14:50:02 +0800

    created general footer layout component file

---
+++ web/components/layouts/GeneralFooterLayout.tsx
@@ -0,0 +1,6 @@
+const footerLinks = [
+  { label: "Delivery Coverage", href: "#support" },
+  { label: "Safety Tips", href: "#support" },
+  { label: "Partner with us", href: "#support" },
+  { label: "Contact", href: "#support" },
+];
