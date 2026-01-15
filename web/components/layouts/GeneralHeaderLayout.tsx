1b3fb4cfec211f733b5dc3325fa51a82dd30430d
rn-santos227 <rn_santos227@yahoo.com>
2026-01-15 14:32:14 +0800

    created general header layout component file

---
+++ web/components/layouts/GeneralHeaderLayout.tsx
@@ -0,0 +1,7 @@
+const navigation = [
+  { label: "Marketplace", href: "#marketplace" },
+  { label: "Categories", href: "#categories" },
+  { label: "Featured", href: "#featured" },
+  { label: "Support", href: "#support" },
+];
+
