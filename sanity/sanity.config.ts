2a5f1e39f365cbe584a4a6ee1f1122b0dd080d07
rn-santos227 <rn_santos227@yahoo.com>
2026-01-15 13:53:58 +0800

    updated santiy config file

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,16 +1,23 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
+
+import { schemaTypes } from './schemaTypes'
+import { structure } from './deskStructure'
 
 export default defineConfig({
   name: 'default',
-  title: 'a4t-lpg-trading',
+  title: 'A4R LPG Trading',
 
   projectId: 'tcpko3be',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [
+    structureTool({
+      structure,
+    }),
+    visionTool(),
+  ],
 
   schema: {
     types: schemaTypes,
