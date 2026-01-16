9e879fc6cce2527e2b0cf0a8edbe766f247e8b24
rn-santos227 <rn_santos227@yahoo.com>
2026-01-16 16:13:29 +0800

    updated home highlight hook file

--- web/hooks/useHomeHighlights.ts
+++ web/hooks/useHomeHighlights.ts
@@ -7,4 +7,28 @@ export type HighlightStat = {
   value: string;
 };
 
+export type BundleHighlight = {
+  title: string;
+  description: string;
+  price: string;
+};
+
+export type FeaturedProduct = {
+  title: string;
+  description: string;
+  price: string;
+  tag: string;
+};
+
+export type ServiceHighlight = {
+  title: string;
+  description: string;
+};
+
+export type SafetyMetric = {
+  label: string;
+  value: string;
+  note: string;
+};
+
 
