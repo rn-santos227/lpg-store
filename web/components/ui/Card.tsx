462dc67fcb4ed53cfa2c96997402306741df6848
rn-santos227 <rn_santos227@yahoo.com>
2026-01-16 15:28:36 +0800

    created card ui component file

---
+++ web/components/ui/Card.tsx
@@ -0,0 +1,8 @@
+import { HTMLAttributes } from "react";
+
+type CardProps = HTMLAttributes<HTMLDivElement>;
+type CardSectionProps = HTMLAttributes<HTMLDivElement>;
+type CardTitleProps = HTMLAttributes<HTMLHeadingElement>;
+type CardDescriptionProps = HTMLAttributes<HTMLParagraphElement>;
+
+
