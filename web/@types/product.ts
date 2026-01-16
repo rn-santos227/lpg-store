7fa48a1e8b57746712e850b7b548fadab58dc040
rn-santos227 <rn_santos227@yahoo.com>
2026-01-16 18:57:26 +0800

    created product type file in web

---
+++ web/@types/product.ts
@@ -0,0 +1,11 @@
+export type Product = {
+  name: string;
+  slug?: string | null;
+  categoryId?: string | null;
+  sizeKg?: number | null;
+  price: number;
+  description?: string | null;
+  imageUrl?: string | null;
+  featured?: boolean | null;
+  available?: boolean | null;
+};
