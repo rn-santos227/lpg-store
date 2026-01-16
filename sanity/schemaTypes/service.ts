1c82abc686280355c2ddf63499dd80d558dd2d39
rn-santos227 <rn_santos227@yahoo.com>
2026-01-16 18:33:12 +0800

    updated service schema in sanity folder

--- sanity/schemaTypes/service.ts
+++ sanity/schemaTypes/service.ts
@@ -5,6 +5,18 @@ export default {
   title: 'Service',
   type: 'document',
   fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: (Rule: Rule) => Rule.required(),
+    },
+    {
+      name: 'slug',
+      type: 'slug',
+      options: { source: 'title' },
+      validation: (Rule: Rule) => Rule.required(),
+    },
 
   ],
 }
