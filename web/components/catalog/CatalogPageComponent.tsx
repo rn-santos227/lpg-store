82ce980617363cff1187b37eebe5ce5ae13a734b
rn-santos227 <rn_santos227@yahoo.com>
2026-01-18 22:06:00 +0800

    updated catalog page component file

--- web/components/catalog/CatalogPageComponent.tsx
+++ web/components/catalog/CatalogPageComponent.tsx
@@ -1,18 +1,34 @@
 import GeneralFooterLayout from "../layouts/GeneralFooterLayout";
 import GeneralHeaderLayout from "../layouts/GeneralHeaderLayout";
 import { ProductCatalogSection } from "./ProductCatalogSection";
-import { productCatalogQuery } from "../../constants/queries";
+import { categoryQuery, productCatalogQuery } from "../../constants/queries";
+import type { Category } from "../../@types/category";
 import type { Product } from "../../@types/product";
 import { fetchSanityQuery } from "../../lib/sanity.api";
 
+type CategoryResult = {
+  _id: string;
+  title: string;
+  slug?: string | null;
+};
+
 type CatalogPageComponentProps = {
+  categoryId?: string;
   searchQuery?: string;
 };
 
 export default async function CatalogPageComponent({
+  categoryId,
   searchQuery,
 }: CatalogPageComponentProps) {
   const products = (await fetchSanityQuery<Product[]>(productCatalogQuery)) ?? [];
+  const categoryResults =
+    (await fetchSanityQuery<CategoryResult[]>(categoryQuery)) ?? [];
+  const categories: Category[] = categoryResults.map((category) => ({
+    id: category._id,
+    title: category.title,
+    slug: category.slug,
+  }));
 
   return (
     <div className="flex min-h-screen flex-col bg-slate-50 text-slate-900">
