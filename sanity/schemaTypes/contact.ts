1a292942707a200d470667c12335cda8bfe26c33
rn-santos227 <rn_santos227@yahoo.com>
2026-01-19 00:46:08 +0800

    updated contact schema in sanity folder

--- sanity/schemaTypes/contact.ts
+++ sanity/schemaTypes/contact.ts
@@ -5,6 +5,24 @@ export default {
   title: 'Contact Information',
   type: 'document',
   fields: [
+    {
+      name: 'address',
+      title: 'Address',
+      type: 'text',
+      validation: (Rule: Rule) => Rule.required().min(10),
+    },
+    {
+      name: 'contactNumber',
+      title: 'Contact Number',
+      type: 'string',
+      validation: (Rule: Rule) => Rule.required(),
+    },
+    {
+      name: 'email',
+      title: 'Support Email',
+      type: 'string',
+      validation: (Rule: Rule) => Rule.required().email(),
+    },
 
   ],
 }
