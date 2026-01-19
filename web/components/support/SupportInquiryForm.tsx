4db0cad77d60bbe5080b0019ec1b460a80db093e
rn-santos227 <rn_santos227@yahoo.com>
2026-01-19 17:25:35 +0800

    updated support inquiry form component file

--- web/components/support/SupportInquiryForm.tsx
+++ web/components/support/SupportInquiryForm.tsx
@@ -18,3 +18,30 @@ const inquiryTopics = [
   "Other",
 ];
 
+export default function SupportInquiryForm() {
+  const { toasts, dismissToast, showError, showSuccess } = useToast();
+  const {
+    values,
+    errors,
+    isSubmitting,
+    isSubmitted,
+    submitError,
+    updateField,
+    handleSubmit,
+  } = useSupportInquiryForm({});
+
+  useEffect(() => {
+    if (submitError) {
+      showError("Inquiry failed", submitError);
+    }
+  }, [showError, submitError]);
+
+  useEffect(() => {
+    if (isSubmitted) {
+      showSuccess(
+        "Inquiry received",
+        "Our support team will reply within 1 business day.",
+      );
+    }
+  }, [isSubmitted, showSuccess]);
+}
